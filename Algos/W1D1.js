560b62b3f79f3048f2c96d68d41e1881a12d9c36
Jim Reeder <reederje@gmail.com>
2021-11-27 11:35:09 -0800

    Initial commit

---
+++ Algos/W1D1.js
@@ -0,0 +1,2 @@
+// Morning Algos Week 1 Day 1.
+// All algos will be completed in JavaScript.
---
+++ LectureCode/python_basics.py
@@ -0,0 +1,12 @@
+'''
+W1D1 - PYTHON BASICS
+- syntax 
+- what about indentation?
+- code blocks?
+'''
+
+# Data types
+# Conditionals
+# Loops
+# Built in methods
+# Functions
